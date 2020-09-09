0aa2c735030f294eb57018a7ffe80d29c737852d
NewbRangerTom <tomthetech34953@gmail.com>
2020-09-09 09:40:58 -0400

    initial upload

---
+++ src/README.md
@@ -0,0 +1,25 @@
+This project is designed to monitor the status of your network.
+In my region we have a lot of network outages during the storm season storm
+I make this so I would have a visual status indicator of my network.
+
+Headers used:
+ESP8266WiFi
+ESP8266Ping
+Arduino
+
+Hardware:
+1. ESP8266 Wemos D1 Mini (or Pro depending on what I have on hand)
+2. Two dual color (green/red) LED's
+   (I used two for some of my holiday decorations but you can more or less)
+3. One 10K ohm resistor (you can change this based on your brighness preferences)
+4. appropriate lengths of wire for your enclosure of choice
+   (I also color coordinated my wire for the LED for ease of identifying)
+5. Shrink tube to insulate the LEDs annode's and cathode
+6. Male header pins for the LEDs
+7. Female headers for the D1 Mini
+
+Wetware:
+1. Some imagination for an enclosure and routing of your power source.
+   (my first one was made to fit inside a 4 inch (10cm ish) diameter halloween decoration)
+
+Have fun with this little project.
---
+++ src/main.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266Ping.h>
+
+const char* WIFI_SSID = "change_this";  //your SSID
+const char* WIFI_PWD = "change_this";  //your password (WPA2 key)
+
+const IPAddress remote_ip(1,1,1,1);
+const IPAddress gateway_ip(172,25,142,129);
+
+int dt1=1000;
+int dt2=2000;
+int dt5=5000;
+
+int ledR = D3;
+int ledG = D4;
+
+long day = 86400000; // 86400000 milliseconds in a day
+long hour = 3600000; // 3600000 milliseconds in an hour
+long minute = 60000; // 60000 milliseconds in a minute
+long second =  1000; // 1000 milliseconds in a second
+
+void printDigits(byte digits){
+ // utility function for digital clock display: prints colon and leading 0
+ Serial.print(":");
+ if(digits < 10)
+   Serial.print('0');
+ Serial.print(digits,DEC);  
+}
+
+void time(){
+  long timeNow = millis();
+ 
+  int days = timeNow / day ;                                //number of days
+  int hours = (timeNow % day) / hour;                       //the remainder from days division (in milliseconds) divided by hours, this gives the full hours
+  int minutes = ((timeNow % day) % hour) / minute ;         //and so on...
+  int seconds = (((timeNow % day) % hour) % minute) / second;
+
+// digital clock display of current time
+   Serial.print(days,DEC);  
+   printDigits(hours);  
+   printDigits(minutes);
+   printDigits(seconds);
+   Serial.println();  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  digitalWrite(ledR, LOW);
+  digitalWrite(ledG, LOW); 
+  
+  WiFi.begin(WIFI_SSID, WIFI_PWD);
+  delay(dt2);
+  
+  int counter = 0;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(dt1);
+    Serial.print("Waiting!");
+  
+    counter++;
+  }
+
+  Serial.println("WiFi connected ");  
+  Serial.println(WiFi.localIP());
+  delay(dt5);
+
+  Serial.print("Ping Gateway IP ");
+  Serial.println(gateway_ip);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if(WiFi.status() == WL_CONNECTED){
+    Serial.print("LAN UP ");
+    time();
+    Serial.println(" ");
+    }
+
+  if(Ping.ping(remote_ip)) {
+    Serial.print("WAN UP ");
+    time();
+    Serial.println(" ");
+    digitalWrite(ledG, HIGH);
+    digitalWrite(ledR, LOW);
+    } 
+  else {
+    Serial.print("WAN Down ");
+    time();
+    Serial.println(" ");
+    digitalWrite(ledG, LOW);
+    digitalWrite(ledR, HIGH);
+    }
+}
